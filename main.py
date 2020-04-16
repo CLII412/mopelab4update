93229d89a7c478e589136dc789d06a19b114a940
CLII412 <sw412fr@gmail.com>
2020-04-16 21:55:06 +0300

    update

--- main.py
+++ main.py
@@ -1,240 +1,257 @@
-import math
-import numpy as np
-from numpy.linalg import solve
+import random
 from scipy.stats import f, t
-from functools import partial
-from random import randint
 from prettytable import PrettyTable
+from numpy.linalg import solve
 
 
-def cohren(f1, f2, q=0.05):
-    q1 = q / f1
-    fisher_value = f.ppf(q=1 - q1, dfn=f2, dfd=(f1 - 1) * f2)
-    return fisher_value / (fisher_value + f1 - 1)
+x1_max = 15
+x1_min = -20
+x2_max = -30
+x2_min = 15
+x3_max = -15
+x3_min = -30
+
+q = 0.05
+
+x_av_max = (x1_max + x2_max + x3_max) / 3
+x_av_min = (x1_min + x2_min + x3_min) / 3
+y_max = int(200 + x_av_max)
+y_min = int(200 + x_av_min)
+
+m = 4
+
+X11 = [-1, -1, -1, -1, 1, 1, 1, 1]
+X22 = [-1, -1, 1, 1, -1, -1, 1, 1]
+X33 = [-1, 1, -1, 1, -1, 1, -1, 1]
 
 
-fisher = partial(f.ppf, q=1-0.05)
-student = partial(t.ppf, q=1-0.025)
+def sum2(x1, x2):
+    xn = []
+    for i in range(len(x1)):
+        xn.append(x1[i] * x2[i])
+    return xn
 
 
-X1max = 15
-X1min = -20
-X2max = -30
-X2min = 15
-X3max = -15
-X3min = -30
+def sum3(x1, x2, x3):
+    xn = []
+    for i in range(len(x1)):
+        xn.append(x1[i] * x2[i] * x3[i])
+    return xn
 
-Xmax_average = (X1max + X2max + X3max)/3
-Xmin_average = (X1min + X2min + X3min)/3
 
-y_max = round(200 + Xmax_average)
-y_min = round(200 + Xmin_average)
+X12 = sum2(X11, X22)
+X13 = sum2(X11, X33)
+X23 = sum2(X22, X33)
+X123 = sum3(X11, X22, X33)
 
-x0_factor = [1, 1, 1, 1, 1, 1, 1, 1]
-x1_factor = [-1, -1, 1, 1, -1, -1, 1, 1]
-x2_factor = [-1, 1, -1, 1, -1, 1, -1, 1]
-x3_factor = [-1, 1, 1, -1, 1, -1, -1, 1]
-x1x2_factor = [a*b for a, b in zip(x1_factor, x2_factor)]
-x1x3_factor = [a*b for a, b in zip(x1_factor, x3_factor)]
-x2x3_factor = [a*b for a, b in zip(x2_factor, x3_factor)]
-x1x2x3_factor = [a*b*c for a, b, c in zip(x1_factor, x2_factor, x3_factor)]
+X00 = [1, 1, 1, 1, 1, 1, 1, 1]
+
+print("y=b0+b1*x1+b2*x2+b3*x3+b12*x1*x2+b13*x1*x3+b23*x2*x3+b123*x1*x2*x3+b11*x1^2+b22*x2^2+b33*x3^2\n")
+print("Кодовані значення X")
+table1 = PrettyTable()
+table1.add_column("№", (1, 2, 3, 4, 5, 6, 7, 8))
+table1.add_column("X1", X11)
+table1.add_column("X2", X22)
+table1.add_column("X3", X33)
+table1.add_column("X12", X12)
+table1.add_column("X13", X13)
+table1.add_column("X23", X23)
+table1.add_column("X123", X123)
+print(table1, "\n")
+
 
 m = 3
+while True:
 
-y1, y2, y3 = [], [], []
-for i in range(0, 8):
-    y1.append(randint(y_min, y_max))
-    y2.append(randint(y_min, y_max))
-    y3.append(randint(y_min, y_max))
+    for i in range(1, m + 1):
+        globals()['Y%s' % i] = [random.randrange(y_min, y_max, 1) for k in range(8)]
+    X1 = [x1_min, x1_min, x1_min, x1_min, x1_max, x1_max, x1_max, x1_max]
+    X2 = [x2_min, x2_min, x2_max, x2_max, x2_min, x2_min, x2_max, x2_max]
+    X3 = [x3_min, x3_max, x3_min, x3_max, x3_min, x3_max, x3_min, x3_max]
+    X12 = sum2(X1, X2)
+    X13 = sum2(X1, X3)
+    X23 = sum2(X2, X3)
+    X123 = sum3(X1, X2, X3)
+    X0 = [1] * 8
+    s1, s2, s3, s4, s5, s6, s7, s8 = 0, 0, 0, 0, 0, 0, 0, 0
+    for i in range(1, m + 1):
+        s1 += globals()['Y%s' % i][0]
+        s2 += globals()['Y%s' % i][1]
+        s3 += globals()['Y%s' % i][2]
+        s4 += globals()['Y%s' % i][3]
+        s5 += globals()['Y%s' % i][4]
+        s6 += globals()['Y%s' % i][5]
+        s7 += globals()['Y%s' % i][6]
+        s8 += globals()['Y%s' % i][7]
 
-Y_row1 = [y1[0], y2[0], y3[0]]
-Y_row2 = [y1[1], y2[1], y3[1]]
-Y_row3 = [y1[2], y2[2], y3[2]]
-Y_row4 = [y1[3], y2[3], y3[3]]
-Y_row5 = [y1[4], y2[4], y3[4]]
-Y_row6 = [y1[5], y2[5], y3[5]]
-Y_row7 = [y1[6], y2[6], y3[6]]
-Y_row8 = [y1[7], y2[7], y3[7]]
+    y1_av1 = s1 / m
+    y2_av2 = s2 / m
+    y3_av3 = s3 / m
+    y4_av4 = s4 / m
+    y5_av5 = s5 / m
+    y6_av6 = s6 / m
+    y7_av7 = s7 / m
+    y8_av8 = s8 / m
+    y_av = [round(y1_av1, 3), round(y2_av2, 3), round(y3_av3, 3), round(y4_av4, 3), round(y5_av5, 3), round(y6_av6, 3),
+            round(y7_av7, 3), round(y8_av8, 3)]
+    sd1, sd2, sd3, sd4, sd5, sd6, sd7, sd8 = 0, 0, 0, 0, 0, 0, 0, 0
+    for i in range(1, m + 1):
+        sd1 += ((globals()['Y%s' % i][0]) - y1_av1) ** 2
+        sd2 += ((globals()['Y%s' % i][1]) - y2_av2) ** 2
+        sd3 += ((globals()['Y%s' % i][2]) - y3_av3) ** 2
+        sd4 += ((globals()['Y%s' % i][3]) - y4_av4) ** 2
+        sd5 += ((globals()['Y%s' % i][4]) - y5_av5) ** 2
+        sd6 += ((globals()['Y%s' % i][5]) - y6_av6) ** 2
+        sd7 += ((globals()['Y%s' % i][6]) - y7_av7) ** 2
+        sd8 += ((globals()['Y%s' % i][7]) - y8_av8) ** 2
 
-Y_average1 = np.average(Y_row1)
-Y_average2 = np.average(Y_row2)
-Y_average3 = np.average(Y_row3)
-Y_average4 = np.average(Y_row4)
-Y_average5 = np.average(Y_row5)
-Y_average6 = np.average(Y_row6)
-Y_average7 = np.average(Y_row7)
-Y_average8 = np.average(Y_row8)
-Y_average = [round(Y_average1, 3), round(Y_average2, 3), round(Y_average3, 3), round(Y_average4, 3),
-             round(Y_average5, 3), round(Y_average6, 3), round(Y_average7, 3), round(Y_average8, 3)]
+    d1 = sd1 / m
+    d2 = sd2 / m
+    d3 = sd3 / m
+    d4 = sd4 / m
+    d5 = sd5 / m
+    d6 = sd6 / m
+    d7 = sd7 / m
+    d8 = sd8 / m
+    disper = [round(d1, 3), round(d2, 3), round(d3, 3), round(d4, 3), round(d5, 3), round(d6, 3), round(d7, 3),
+              round(d8, 3)]
 
-x0 = [1, 1, 1, 1, 1, 1, 1, 1]
-x1 = [-25, -25, -5, -5, -25, -25, -5, -5]
-x2 = [-70, -10, -70, -10, -70, -10, -70, -10]
-x3 = [-25, -5, -5, -25, -5, -25, -25, -5]
-x1x2 = [a*b for a, b in zip(x1, x2)]
-x1x3 = [a*b for a, b in zip(x1, x3)]
-x2x3 = [a*b for a, b in zip(x2, x3)]
-x1x2x3 = [a*b*c for a, b, c in zip(x1, x2, x3)]
+    print("Матриця планування:")
+    table2 = PrettyTable()
+    table2.add_column("№", (1, 2, 3, 4, 5, 6, 7, 8))
+    table2.add_column("X1", X1)
+    table2.add_column("X2", X2)
+    table2.add_column("X3", X3)
+    table2.add_column("X12", X12)
+    table2.add_column("X13", X13)
+    table2.add_column("X23", X23)
+    table2.add_column("X123", X123)
+    for i in range(1, m + 1):
+        table2.add_column("Y" + str(i), globals()['Y%s' % i])
+    table2.add_column("Y", y_av)
+    table2.add_column("S^2", disper)
+    print(table2, "\n")
 
-list_for_solve_b = [x0_factor, x1_factor, x2_factor, x3_factor, x1x2_factor, x1x3_factor, x2x3_factor, x1x2x3_factor]
-list_for_solve_a = list(zip(x0, x1, x2, x3, x1x2, x1x3, x2x3, x1x2x3))
+    b = [i for i in solve(list(zip(X0, X1, X2, X3, X12, X13, X23, X123)), y_av)]
+    b0, b1, b2, b3, b4, b5, b6, b7 = round(b[0], 3), round(b[1], 3), round(b[2], 3), round(b[3], 3), round(b[4],
+                                                                                                           3), round(
+        b[5], 3), round(b[6], 3), round(b[7], 3)
+    print("y=" + str(b0) + "+" + str(b1) + "*x1+" + str(b2) + "*x2+" + str(b3) + "*x3+" + str(b4) + "*x1*x2+" + str(
+        b5) + "*x1*x3+" + str(b6) + "*x2*x3+" + str(b7) + "*x1*x2*x3", "\n")
 
-N = 8
-list_bi = []
-for k in range(N):
-    S = 0
-    for i in range(N):
-        S += (list_for_solve_b[k][i]*Y_average[i])/N
-    list_bi.append(round(S, 5))
+    dcouple = [d1, d2, d3, d4, d5, d6, d7, d8]
 
-Disp1 = 0
-Disp2 = 0
-Disp3 = 0
-Disp4 = 0
-Disp5 = 0
-Disp6 = 0
-Disp7 = 0
-Disp8 = 0
-for i in range(m):
-    Disp1 += ((Y_row1[i] - np.average(Y_row1))**2)/m
-    Disp2 += ((Y_row2[i] - np.average(Y_row2))**2)/m
-    Disp3 += ((Y_row3[i] - np.average(Y_row3))**2)/m
-    Disp4 += ((Y_row4[i] - np.average(Y_row4))**2)/m
-    Disp5 += ((Y_row5[i] - np.average(Y_row5))**2)/m
-    Disp6 += ((Y_row6[i] - np.average(Y_row6))**2)/m
-    Disp7 += ((Y_row7[i] - np.average(Y_row7))**2)/m
-    Disp8 += ((Y_row8[i] - np.average(Y_row8))**2)/m
-sum_dispersion = Disp1 + Disp2 + Disp3 + Disp4 + Disp5 + Disp6 + Disp7 + Disp8
-disp_list = [round(Disp1, 3), round(Disp2, 3), round(Disp3, 3), round(Disp4, 3), round(Disp5, 3),  round(Disp6, 3),
-             round(Disp7, 3), round(Disp8, 3)]
+    print("Критерій Кохрена")
 
-pt1 = PrettyTable()
-column_names1 = ["X0", "X1", "X2", "X3", "X1X2", "X1X3", "X2X3", "X1X2X3", "Y1", "Y2", "Y3", "Y", "S^2"]
-pt1.add_column(column_names1[0], x0_factor)
-pt1.add_column(column_names1[1], x1_factor)
-pt1.add_column(column_names1[2], x2_factor)
-pt1.add_column(column_names1[3], x3_factor)
-pt1.add_column(column_names1[4], x1x2_factor)
-pt1.add_column(column_names1[5], x1x3_factor)
-pt1.add_column(column_names1[6], x2x3_factor)
-pt1.add_column(column_names1[7], x1x2x3_factor)
-pt1.add_column(column_names1[8], y1)
-pt1.add_column(column_names1[9], y2)
-pt1.add_column(column_names1[10], y3)
-pt1.add_column(column_names1[11], Y_average)
-pt1.add_column(column_names1[12], disp_list)
-print(pt1, "\n")
+    Gp = max(dcouple) / sum(dcouple)
+    f1 = m - 1
+    f2 = N = 8
+    fisher = f.isf(*[q / f2, f1, (f2 - 1) * f1])
+    Gt = round(fisher / (fisher + (f2 - 1)), 4)
+    print("Gp = " + str(Gp) + ", Gt = " + str(Gt))
+    if Gp < Gt:
+        print("Gp < Gt -> Дисперсія однорідна\n")
+        print("Критерій Стьюдента")
+        sb = sum(dcouple) / N
+        ssbs = sb / N * m
+        sbs = ssbs ** 0.5
 
-print("y = {} + {}*x1 + {}*x2 + {}*x3 + {}*x1x2 + {}*x1x3 + {}*x2x3 + {}*x1x2x3 \n".format(list_bi[0], list_bi[1],
-                                                                                           list_bi[2], list_bi[3],
-                                                                                           list_bi[4], list_bi[5],
-                                                                                           list_bi[6], list_bi[7]))
+        beta0 = (y1_av1 * 1 + y2_av2 * 1 + y3_av3 * 1 + y4_av4 * 1 + y5_av5 * 1 + y6_av6 * 1 + y7_av7 * 1 + y8_av8 * 1) / 8
+        beta1 = (y1_av1 * (-1) + y2_av2 * (-1) + y3_av3 * (-1) + y4_av4 * (
+            -1) + y5_av5 * 1 + y6_av6 * 1 + y7_av7 * 1 + y8_av8 * 1) / 8
+        beta2 = (y1_av1 * (-1) + y2_av2 * (-1) + y3_av3 * 1 + y4_av4 * 1 + y5_av5 * (-1) + y6_av6 * (
+            -1) + y7_av7 * 1 + y8_av8 * 1) / 8
+        beta3 = (y1_av1 * (-1) + y2_av2 * 1 + y3_av3 * (-1) + y4_av4 * 1 + y5_av5 * (-1) + y6_av6 * 1 + y7_av7 * (
+            -1) + y8_av8 * 1) / 8
+        beta4 = (y1_av1 * 1 + y2_av2 * 1 + y3_av3 * (-1) + y4_av4 * (-1) + y5_av5 * (-1) + y6_av6 * (
+            -1) + y7_av7 * 1 + y8_av8 * 1) / 8
+        beta5 = (y1_av1 * 1 + y2_av2 * (-1) + y3_av3 * 1 + y4_av4 * (-1) + y5_av5 * (-1) + y6_av6 * 1 + y7_av7 * (
+            -1) + y8_av8 * 1) / 8
+        beta6 = (y1_av1 * 1 + y2_av2 * (-1) + y3_av3 * (-1) + y4_av4 * 1 + y5_av5 * 1 + y6_av6 * (-1) + y7_av7 * (
+            -1) + y8_av8 * 1) / 8
+        beta7 = (y1_av1 * (-1) + y2_av2 * 1 + y3_av3 * 1 + y4_av4 * (-1) + y5_av5 * 1 + y6_av6 * (-1) + y7_av7 * (
+            -1) + y8_av8 * 1) / 8
 
-pt2 = PrettyTable()
-pt2.add_column(column_names1[0], x0)
-pt2.add_column(column_names1[1], x1)
-pt2.add_column(column_names1[2], x2)
-pt2.add_column(column_names1[3], x3)
-pt2.add_column(column_names1[4], x1x2)
-pt2.add_column(column_names1[5], x1x3)
-pt2.add_column(column_names1[6], x2x3)
-pt2.add_column(column_names1[7], x1x2x3)
-pt2.add_column(column_names1[8], y1)
-pt2.add_column(column_names1[9], y2)
-pt2.add_column(column_names1[10], y3)
-pt2.add_column(column_names1[11], Y_average)
-pt2.add_column(column_names1[12], disp_list)
-print(pt2, '\n')
+        t0 = abs(beta0) / sbs
+        t1 = abs(beta1) / sbs
+        t2 = abs(beta2) / sbs
+        t3 = abs(beta3) / sbs
+        t4 = abs(beta4) / sbs
+        t5 = abs(beta5) / sbs
+        t6 = abs(beta6) / sbs
+        t7 = abs(beta7) / sbs
 
-list_ai = [round(i, 5) for i in solve(list_for_solve_a, Y_average)]
-print("y = {} + {}*x1 + {}*x2 + {}*x3 + {}*x1x2 + {}*x1x3 + {}*x2x3 + {}*x1x2x3".format(list_ai[0], list_ai[1],
-                                                                                        list_ai[2], list_ai[3],
-                                                                                        list_ai[4], list_ai[5],
-                                                                                        list_ai[6], list_ai[7]))
+        f3 = f1 * f2
+        ttabl = round(abs(t.ppf(q / 2, f3)), 4)
 
-Gp = max(Disp1, Disp2, Disp3, Disp4, Disp5, Disp6, Disp7, Disp8) / sum_dispersion
-F1 = m-1
-N = len(y1)
-F2 = N
-Gt = cohren(F1, F2)
-print("\nGp = ", Gp, " Gt = ", Gt)
-if Gp < Gt:
-    print("Дисперсія однорідна!\n")
+        d = 8
+        if (t0 < ttabl):
+            print("t0 < ttabl, b0 не значимий")
+            b0 = 0
+            d = d - 1
+        if (t1 < ttabl):
+            print("t1 < ttabl, b1 не значимий")
+            b1 = 0
+            d = d - 1
+        if (t2 < ttabl):
+            print("t2 < ttabl, b2 не значимий")
+            b2 = 0
+            d = d - 1
+        if (t3 < ttabl):
+            print("t3 < ttabl, b3 не значимий")
+            b3 = 0
+            d = d - 1
+        if (t4 < ttabl):
+            print("t4 < ttabl, b4 не значимий")
+            b4 = 0
+            d = d - 1
+        if (t5 < ttabl):
+            print("t5 < ttabl, b5 не значимий")
+            b5 = 0
+            d = d - 1
+        if (t6 < ttabl):
+            print("t6 < ttabl, b6 не значимий")
+            b6 = 0
+            d = d - 1
+        if (t7 < ttabl):
+            print("t7 < ttabl, b7 не значимий\n")
+            b7 = 0
+            d = d - 1
 
-    Dispersion_B = sum_dispersion / N
-    Dispersion_beta = Dispersion_B / (m * N)
-    S_beta = math.sqrt(abs(Dispersion_beta))
+        yy1 = b0 + b1 * x1_min + b2 * x2_min + b3 * x3_min + b4 * x1_min * x2_min + b5 * x1_min * x3_min + b6 * x2_min * \
+              x3_min + b7 * x1_min * x2_min * x3_min
+        yy2 = b0 + b1 * x1_min + b2 * x2_min + b3 * x3_max + b4 * x1_min * x2_min + b5 * x1_min * x3_max + b6 * x2_min * \
+              x3_max + b7 * x1_min * x2_min * x3_max
+        yy3 = b0 + b1 * x1_min + b2 * x2_max + b3 * x3_min + b4 * x1_min * x2_max + b5 * x1_min * x3_min + b6 * x2_max * \
+              x3_min + b7 * x1_min * x2_max * x3_min
+        yy4 = b0 + b1 * x1_min + b2 * x2_max + b3 * x3_max + b4 * x1_min * x2_max + b5 * x1_min * x3_max + b6 * x2_max * \
+              x3_max + b7 * x1_min * x2_max * x3_max
+        yy5 = b0 + b1 * x1_max + b2 * x2_min + b3 * x3_min + b4 * x1_max * x2_min + b5 * x1_max * x3_min + b6 * x2_min * \
+              x3_min + b7 * x1_max * x2_min * x3_min
+        yy6 = b0 + b1 * x1_max + b2 * x2_min + b3 * x3_max + b4 * x1_max * x2_min + b5 * x1_max * x3_max + b6 * x2_min * \
+              x3_max + b7 * x1_max * x2_min * x3_max
+        yy7 = b0 + b1 * x1_max + b2 * x2_max + b3 * x3_min + b4 * x1_max * x2_max + b5 * x1_max * x3_min + b6 * x2_max * \
+              x3_min + b7 * x1_max * x2_max * x3_min
+        yy8 = b0 + b1 * x1_max + b2 * x2_max + b3 * x3_max + b4 * x1_max * x2_max + b5 * x1_max * x3_max + b6 * x2_max * \
+              x3_max + b7 * x1_max * x2_max * x3_max
+        print("Критерій Фішера")
+        print(d, " значимих коефіцієнтів")
+        f4 = N - d
+        sad = ((yy1 - y1_av1) ** 2 + (yy2 - y2_av2) ** 2 + (yy3 - y3_av3) ** 2 + (yy4 - y4_av4) ** 2 + (
+                yy5 - y5_av5) ** 2 + (
+                       yy6 - y6_av6) ** 2 + (yy7 - y7_av7) ** 2 + (yy8 - y8_av8) ** 2) * (m / (N - d))
+        Fp = sad / sb
+        print("Fp=", round(Fp, 2))
 
-    beta0 = 0
-    beta1 = 0
-    beta2 = 0
-    beta3 = 0
-    beta4 = 0
-    beta5 = 0
-    beta6 = 0
-    beta7 = 0
-    for i in range(len(x0_factor)):
-        beta0 += (Y_average[i] * x0_factor[i]) / N
-        beta1 += (Y_average[i] * x1_factor[i]) / N
-        beta2 += (Y_average[i] * x2_factor[i]) / N
-        beta3 += (Y_average[i] * x3_factor[i]) / N
-        beta4 += (Y_average[i] * x1x2_factor[i]) / N
-        beta5 += (Y_average[i] * x1x3_factor[i]) / N
-        beta6 += (Y_average[i] * x2x3_factor[i]) / N
-        beta7 += (Y_average[i] * x1x2x3_factor[i]) / N
-    beta_list = [beta0, beta1, beta2, beta3, beta4, beta5, beta6, beta7]
+        Ft = round(abs(f.isf(q, f4, f3)), 4)
 
-    t0 = abs(beta0) / S_beta
-    t1 = abs(beta1) / S_beta
-    t2 = abs(beta2) / S_beta
-    t3 = abs(beta3) / S_beta
-    t4 = abs(beta4) / S_beta
-    t5 = abs(beta5) / S_beta
-    t6 = abs(beta6) / S_beta
-    t7 = abs(beta7) / S_beta
-    t_list = [t0, t1, t2, t3, t4, t5, t6, t7]
-
-    F3 = F1 * F2
-    d = 0
-    T = student(df=F3)
-    print("t табличне = ", T)
-    for i in range(len(t_list)):
-        if t_list[i] < T:
-            beta_list[i] = 0
-            print("Гіпотеза підтверджена, beta{} = 0".format(i))
+        print("Fp = " + str(round(Fp, 2)) + ", Ft = " + str(Ft))
+        if Fp < Ft:
+            print("Fp > Ft -> Рівняння адекватне оригіналу")
+            break
         else:
-            print("Гіпотеза не підтверджена.\nbeta{} = {}".format(i, beta_list[i]))
-            d += 1
-
-    y_1 = beta_list[0] + beta_list[1] * x1[0] + beta_list[2] * x2[0] + beta_list[3] * x3[0] + beta_list[4] * x1x2[0] \
-          + beta_list[5]*x1x3[0] + beta_list[6]*x2x3[0] + beta_list[7]*x1x2x3[0]
-    y_2 = beta_list[0] + beta_list[1] * x1[1] + beta_list[2] * x2[1] + beta_list[3] * x3[1] + beta_list[4] * x1x2[1] \
-          + beta_list[5] * x1x3[1] + beta_list[6] * x2x3[1] + beta_list[7] * x1x2x3[1]
-    y_3 = beta_list[0] + beta_list[1] * x1[2] + beta_list[2] * x2[2] + beta_list[3] * x3[2] + beta_list[4] * x1x2[2] \
-          + beta_list[5] * x1x3[2] + beta_list[6] * x2x3[2] + beta_list[7] * x1x2x3[2]
-    y_4 = beta_list[0] + beta_list[1] * x1[3] + beta_list[2] * x2[3] + beta_list[3] * x3[3] + beta_list[4] * x1x2[3] \
-          + beta_list[5] * x1x3[3] + beta_list[6] * x2x3[3] + beta_list[7] * x1x2x3[3]
-    y_5 = beta_list[0] + beta_list[1] * x1[4] + beta_list[2] * x2[4] + beta_list[3] * x3[4] + beta_list[4] * x1x2[4] \
-          + beta_list[5] * x1x3[4] + beta_list[6] * x2x3[4] + beta_list[7] * x1x2x3[4]
-    y_6 = beta_list[0] + beta_list[1] * x1[5] + beta_list[2] * x2[5] + beta_list[3] * x3[5] + beta_list[4] * x1x2[5] \
-          + beta_list[5] * x1x3[5] + beta_list[6] * x2x3[5] + beta_list[7] * x1x2x3[5]
-    y_7 = beta_list[0] + beta_list[1] * x1[6] + beta_list[2] * x2[6] + beta_list[3] * x3[6] + beta_list[4] * x1x2[6] \
-          + beta_list[5] * x1x3[6] + beta_list[6] * x2x3[6] + beta_list[7] * x1x2x3[6]
-    y_8 = beta_list[0] + beta_list[1] * x1[7] + beta_list[2] * x2[7] + beta_list[3] * x3[7] + beta_list[4] * x1x2[7] \
-          + beta_list[5] * x1x3[7] + beta_list[6] * x2x3[7] + beta_list[7] * x1x2x3[7]
-    Y_counted_for_Student = [y_1, y_2, y_3, y_4, y_5, y_6, y_7, y_8]
-
-    F4 = N - d
-    Dispersion_ad = 0
-    for i in range(len(Y_counted_for_Student)):
-        Dispersion_ad += ((Y_counted_for_Student[i] - Y_average[i]) ** 2) * m / (N - d)
-    Fp = Dispersion_ad / Dispersion_beta
-    Ft = fisher(dfn=F4, dfd=F3)
-    if Fp > Ft:
-        print("Рівняння регресії неадекватне.")
+            print("Fp < Ft -> Рівняння неадекватне оригіналу")
+            m = m+1
     else:
-        print("Рівняння регресії адекватне!")
-
-else:
-    print("Дисперсія неоднорідна. Спробуйте ще раз.")
-
+        print("Дисперсія неоднорідна (Gp > Gt), збільшуємо m")
+        m += 1
